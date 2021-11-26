e6ea8836e6c9a4b8204f792124863c2c4af1a2a8
Fellow Worker <fellow-worker@outlook.com>
2021-11-26 17:19:49 +0100

    Added Viemo & Youtube to exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { MarkDown } from './components/MarkDown/MarkDown'
 export { TextEditor } from './components/TextEditor/TextEditor'
 export { BaseStyle } from './components/Base/BaseStyle'
+export { Youtube, Vimeo } from './components/Base'
