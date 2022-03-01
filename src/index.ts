47e657870627dea70459bb77d3af1c195d6d29b5
fellow-worker <62179490+fellow-worker@users.noreply.github.com>
2022-03-01 16:24:23 +0100

    adding bible information

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
+import { books } from './util/verse/books'
+
 export { MarkDown } from './components/MarkDown/MarkDown'
 export { TextEditor } from './components/TextEditor/TextEditor'
 export { BaseStyle } from './components/Base/BaseStyle'
 export { Youtube, Vimeo, Video } from './components/Base'
+export { chapters as BibleChapters } from './util/verse/chapters'
+
+export const Bibles = { 'nl' : books };
